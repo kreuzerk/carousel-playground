ed20896fd7fc10a9faff2509edbad5ee3ea0f769
kreuzerk <kevin.kreuzer90@icloud.com>
2023-01-20 13:48:21 +0100

    feat: 🎸 setup and initial features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'carousel-playground';
+export class AppComponent implements OnInit {
+  @ViewChild('mainCard', {static: true}) mainCard!: ElementRef;
+  @ViewChild('leftCard', {static: true}) leftCard!: ElementRef;
+  @ViewChild('rightCard', {static: true}) rightCard!: ElementRef;
+
+  private images = ['land', 'mountain', 'sea'];
+
+  ngOnInit(): void {
+    this.swopImages();
+  }
+
+  backwards() {
+    const firstElement = this.images.shift();
+    this.images = [...this.images, firstElement!];
+    this.swopImages();
+  }
+
+  forward() {
+    const lastElement = this.images.pop();
+    this.images = [lastElement!, ...this.images];
+    this.swopImages();
+  }
+
+  private swopImages() {
+    this.mainCard.nativeElement.style.backgroundImage = `url("../assets/${this.images[1]}.jpg")`;
+    this.leftCard.nativeElement.style.backgroundImage = `url("../assets/${this.images[0]}.jpg")`;
+    this.rightCard.nativeElement.style.backgroundImage = `url("../assets/${this.images[2]}.jpg")`;
+  }
+
+
 }
